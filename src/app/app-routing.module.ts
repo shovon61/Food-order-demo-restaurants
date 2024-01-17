53b6b5acca8793625606cb2c673db3944169e72d
shovon61 <mushfiq.sh@gmail.com>
2024-01-17 16:45:21 +0600

    Project Upload to github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HotelsComponent } from './components/hotels/hotels.component';
+import { HotelComponent } from './components/hotel/hotel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'',redirectTo:'/hotels',pathMatch:'full' },
+  { path:'hotels',component:HotelsComponent },
+  { path:'',component:HotelComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/hotel-card/hotel-card.component.ts
@@ -0,0 +1,23 @@
+import { Component,OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-hotel-card',
+  templateUrl: './hotel-card.component.html',
+  styleUrls: ['./hotel-card.component.scss']
+})
+export class HotelCardComponent {
+
+  @Input() public hotelName;
+  @Input() public hotelThumbnail;
+  @Input() public hotelImage;
+  @Input() public cuisines;
+  @Input() public rating;
+  @Input() public review;
+
+  public math = Math;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
